e5503f465a12cfc8a6725eb7d2b643710f772b0a
max-cha2 <mcha611@gmail.com>
2022-07-13 11:12:18 -0700

    finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (num1 < 0 || num2 < 0){
+        return 'ERROR';
+    }
+    if(typeof(num1) != typeof(num2)){
+        return 'ERROR';
+    }
+    if (num1 < num2){
+        for (let i = num1; i <= num2; i++){
+            sum+=i;
+        }
+    }
+    else if(num2 < num1){
+        for (let i = num2; i <= num1; i++){
+            sum+=i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let x = 0;
+  if (arr.length === 0){
+    return 0;
+  }
+	for (let i = 0; i < arr.length; i++){
+    x += arr[i];
+  }
+  return x;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let mult = 1;
+  for (let i = 0; i < arr.length; i++){
+    mult *= arr[i];
+  }
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  let power = 1;
+  for (let i = 0; i < y; i++){
+    power *= x;
+  }
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = 1;
+  if (num === 0){
+    return 1;
+  }
+  for (let i = num; i > 0;){
+    fact *= i;
+    i--;
+  }
+  return fact;
 };
 
 // Do not edit below this line
